068e863934f36dad237a07eaf3ea1656d035ca70
abdul-34 <143192960+abdul-34@users.noreply.github.com>
2025-10-05 15:22:31 +0500

    FIX: CORS ISSUE

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Match all API routes and specific static paths that could be used by Next.js
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://stripe-subscription-manager.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, rsc" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
